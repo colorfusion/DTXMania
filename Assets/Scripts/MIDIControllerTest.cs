474b2a5d4456c56440ec99041f5678fa5b39f12b
Melvin Yeo <1024128+colorfusion@users.noreply.github.com>
2021-12-22 11:38:40 +0800

    Add midi controller test scripts

--- Assets/Scripts/MIDIControllerTest.cs
+++ Assets/Scripts/MIDIControllerTest.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Multimedia;
 
 public class MIDIControllerTest : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(InputDevice.DeviceCount);
     }
 
     // Update is called once per frame
