38960df029d2e0ad7dc2b75ff2207e4c1bceba3d
Melvin Yeo <1024128+colorfusion@users.noreply.github.com>
2021-12-27 12:15:12 +0800

    Add basic function to load InputDevice

--- Assets/Scripts/MIDIControllerTest.cs
+++ Assets/Scripts/MIDIControllerTest.cs
@@ -1,19 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using Multimedia;
 
 public class MIDIControllerTest : MonoBehaviour
 {
+    InputDevice inputDevice;
+    string inputDeviceName;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(InputDevice.DeviceCount);
+        int inputDeviceCount = InputDevice.DeviceCount;
+        Debug.Log(inputDeviceCount);
+
+        if (inputDeviceCount >= 1)
+        {
+            MidiInCaps inputCap = InputDevice.GetCapabilities(0);
+            inputDeviceName = Encoding.UTF8.GetString(inputCap.name);
+
+            Debug.Log(string.Format("Loading {0}",inputDeviceName));
+            inputDevice = new InputDevice(0);
+            inputDevice.Start();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnDestroy()
+    {
+        if (inputDevice != null)
+        {
+            inputDevice.Dispose();
+        }
     }
 }
