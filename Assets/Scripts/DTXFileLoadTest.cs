8453232e0dd03a11436e8f765cf7ee04fa35a7d3
Melvin Yeo <1024128+colorfusion@users.noreply.github.com>
2021-12-31 15:55:08 +0800

    Add test function load audioclip from local disk

--- Assets/Scripts/DTXFileLoadTest.cs
+++ Assets/Scripts/DTXFileLoadTest.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class DTXFileLoadTest : MonoBehaviour
 {
@@ -8,6 +9,33 @@ public class DTXFileLoadTest : MonoBehaviour
     void Start()
     {
         DTXInputOutput dtxIO = DTXInputOutput.Load("Sing Alive (Full Version)/adv.dtx");
+        StartCoroutine(PlayPreview(dtxIO.musicInfo.PreviewSound));
+    }
+
+    IEnumerator PlayPreview(string audioPath)
+    {
+        AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            Debug.Log("Adding audio source");
+            // add audio source if it does not exist
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        using(UnityWebRequest uwr = UnityWebRequestMultimedia.GetAudioClip(audioPath, AudioType.OGGVORBIS))
+        {
+            yield return uwr.SendWebRequest();
+            
+            if (uwr.result != UnityWebRequest.Result.Success) {
+                Debug.LogError(uwr.error);
+                yield break;
+            }
+
+            Debug.Log(string.Format("Playing {0}", audioPath));
+            AudioClip clip = DownloadHandlerAudioClip.GetContent(uwr);
+            // use audio clip
+            audioSource.PlayOneShot(clip);
+        }
     }
 
     // Update is called once per frame
