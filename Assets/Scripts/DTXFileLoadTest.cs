a9b3559bc07d9f087e81cf8afbad95f4ba09b147
Melvin Yeo <1024128+colorfusion@users.noreply.github.com>
2022-01-07 01:50:06 +0800

    Remove old test code

--- Assets/Scripts/DTXFileLoadTest.cs
+++ Assets/Scripts/DTXFileLoadTest.cs
@@ -12,23 +12,7 @@ public class DTXFileLoadTest : MonoBehaviour
     void Start()
     {
         dtxIO = GetComponent<DTXInputOutput>();
-        dtxIO.LoadFile("Sing Alive (Full Version)/test.dtx");
-
-        AudioSource audioSource = gameObject.GetComponent<AudioSource>();
-        if (audioSource == null)
-        {
-            Debug.Log("Adding audio source");
-            // add audio source if it does not exist
-            audioSource = gameObject.AddComponent<AudioSource>();
-        }
-
-        string filePath = dtxIO.GetFileAbsolutePath(dtxIO.musicInfo.PreviewSound);
-        StartCoroutine(DTXHelper.GetAudioClip(filePath, (audioClip) => {
-            // Debug.Log(string.Format("Playing {0}", dtxIO.musicInfo.PreviewSound));
-            // audioSource.PlayOneShot(audioClip);
-        }, (_) => {
-            
-        }));
+        dtxIO.LoadFile("Sing Alive (Full Version)/mstr.dtx");
     }
 
     // Update is called once per frame
